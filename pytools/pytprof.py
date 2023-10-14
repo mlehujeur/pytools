75ef5242730ef467bfc8ab780cedb2bcefdfb428
mlehujeur <maximilien.lehujeur@gmail.com>
2023-10-14 10:43:20 +0200

    change scripts behavior, cleanup

--- pytools/pytprof.py
+++ pytools/pytprof.py
@@ -11,6 +11,7 @@ profiling_file = os.path.basename(python_script.split()[0])
 assert profiling_file.endswith('.py')
 profiling_file = profiling_file.replace('.py', '.py.lprof')
 
-cmd = f"/usr/bin/env kernprof -l {python_script} && /usr/bin/env python -m line_profiler {profiling_file}" 
+# cmd = f"/usr/bin/env kernprof -l {python_script} && /usr/bin/env python -m line_profiler {profiling_file}"
+cmd = f"/usr/bin/env kernprof -l -v -u 1e-3 {python_script}"  
 print(cmd)
 os.system(cmd)
