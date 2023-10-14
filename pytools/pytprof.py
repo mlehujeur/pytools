4f0b6c522066cf7b2f29c2faf274d0fe281378f8
mlehujeur <maximilien.lehujeur@gmail.com>
2023-10-14 10:54:04 +0200

    build doc using sphinx

--- pytools/pytprof.py
+++ pytools/pytprof.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 import os, sys, glob
 
-if not len(sys.argv) > 1:
-    raise ValueError('need a python script to run')
 
-# this_file = os.path.basename(__file__)
+if __name__ == "__main__":
+    if not len(sys.argv) > 1:
+        raise ValueError('need a python script to run')
 
-python_script = " ".join(sys.argv[1:])
-profiling_file = os.path.basename(python_script.split()[0])
-assert profiling_file.endswith('.py')
-profiling_file = profiling_file.replace('.py', '.py.lprof')
+    # this_file = os.path.basename(__file__)
 
-cmd = f"/usr/bin/env kernprof -l {python_script} && /usr/bin/env python -m line_profiler {profiling_file}" 
-print(cmd)
-os.system(cmd)
+    python_script = " ".join(sys.argv[1:])
+    profiling_file = os.path.basename(python_script.split()[0])
+    assert profiling_file.endswith('.py')
+    profiling_file = profiling_file.replace('.py', '.py.lprof')
+
+    cmd = f"/usr/bin/env kernprof -l {python_script} && /usr/bin/env python -m line_profiler {profiling_file}" 
+    print(cmd)
+    os.system(cmd)
